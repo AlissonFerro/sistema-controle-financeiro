a8e54c89fd79a635f72077ba55e30660edb890b8
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2022-05-16 21:29:09 -0300

    inicio

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+        private readonly FinanceiroContext _context;
+
+        public HomeController(FinanceiroContext context)
+        {
+            _context = context;
+        }*/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Pagamento.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Pagamento
+    {
+        // id, descrição do pagamento, valor, código de barras, data de vencimento e data de pagamento.
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public string CodBarras { get; set; }
+        public DateOnly DataVencimento { get; set; }
+        public DateOnly DataPagamento { get; set; }
+    }
+}
